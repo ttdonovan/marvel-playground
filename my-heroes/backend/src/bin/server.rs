6ad647bcf3cdcaa399ff8ee5c500c792b2b7629e
Tanner Donovan <ttdonovan@gmail.com>
2019-09-06 16:05:15 -0700

    add a simple router

--- my-heroes/backend/src/bin/server.rs
+++ my-heroes/backend/src/bin/server.rs
@@ -1,6 +1,10 @@
-use futures::{Future, Stream};
+// use futures::{Future, Stream};
 use gotham::{
     helpers::http::response::create_response,
+    router::{
+        builder::*,
+        Router,
+    },
     state::State,
 };
 use hyper::{Body, Response, StatusCode};
@@ -10,33 +14,42 @@ fn say_hello(state: State) -> (State, Response<Body>) {
         &state,
         StatusCode::OK,
         mime::TEXT_PLAIN,
-        String::from("My Hereos!"),
+        String::from("My Heroes!"),
     );
 
     (state, res)
 }
 
+fn router() -> Router {
+    build_simple_router(|route| {
+        route.get("/").to(say_hello);
+    })
+}
+
 fn main() {
     let addr = "127.0.0.1:7878";
+    println!("Listening for requests at http://{}", addr);
 
-    let server = gotham::init_server(addr, || Ok(say_hello));
+    gotham::start(addr, router())
 
-    // Future to wait for Ctrl+C
-    let signal = tokio_signal::ctrl_c()
-        .flatten_stream()
-        .map_err(|error| panic!("Error listening for signal: {}", error))
-        .take(1)
-        .for_each(|()| {
-            println!("Ctrl+C pressed - Goodbye!");
-            Ok(())
-        });
+    // let server = gotham::init_server(addr, router());
 
-    let serve_until = signal
-        .select(server)
-        .map(|(res, _)| res)
-        .map_err(|(error, _)| error);
+    // // Future to wait for Ctrl+C
+    // let signal = tokio_signal::ctrl_c()
+    //     .flatten_stream()
+    //     .map_err(|error| panic!("Error listening for signal: {}", error))
+    //     .take(1)
+    //     .for_each(|()| {
+    //         println!("Ctrl+C pressed - Goodbye!");
+    //         Ok(())
+    //     });
 
-    tokio::run(serve_until);
+    // let serve_until = signal
+    //     .select(server)
+    //     .map(|(res, _)| res)
+    //     .map_err(|(error, _)| error);
 
-    println!("Shutting down gracefully.");
+    // tokio::run(serve_until);
+
+    // println!("Shutting down gracefully.");
 }
