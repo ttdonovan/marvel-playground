90e0d2e7150e3322b6cf693c3ad17e8058648639
Tanner Donovan <ttdonovan@gmail.com>
2019-09-06 16:45:58 -0700

    add query string to /heroes?name=Hulk

--- my-heroes/backend/src/bin/server.rs
+++ my-heroes/backend/src/bin/server.rs
@@ -1,14 +1,27 @@
+#[macro_use]
+extern crate gotham_derive;
+#[macro_use]
+extern crate serde_derive;
+
 // use futures::{Future, Stream};
-use gotham::router::{
-    builder::*,
-    Router,
+
+use gotham::{
+    router::{
+        builder::*,
+        Router,
+    },
 };
 
-pub mod handlers {
+#[derive(Deserialize, StateData, StaticResponseExtender)]
+struct QueryStringExtractor {
+    name: Option<String>,
+}
+
+mod handlers {
     use hyper::{Body, Response, StatusCode};
     use gotham::{
         helpers::http::response::create_response,
-        state::State,
+        state::{FromState, State},
     };
 
     pub fn index(state: State) -> (State, Response<Body>) {
@@ -23,27 +36,49 @@ pub mod handlers {
     }
 
     pub mod heroes {
+        use crate::QueryStringExtractor;
         use super::*;
 
-        pub fn index(state: State) -> (State, Response<Body>) {
-            let res = create_response(
-                &state,
-                StatusCode::OK,
-                mime::TEXT_PLAIN,
-                String::from("List Heroes!"),
-            );
+        pub fn index(mut state: State) -> (State, Response<Body>) {
+            let res = {
+                let query_param = QueryStringExtractor::take_from(&mut state);
+
+                let msg = match query_param.name {
+                    Some(name) => { format!("Hero: {}", name) },
+                    None => { format!("List All Heroes!") },
+                };
+
+                create_response(
+                    &state,
+                    StatusCode::OK,
+                    mime::TEXT_PLAIN,
+                    msg,
+                )
+            };
 
             (state, res)
         }
     }
 }
 
+/// Create a `Router`
+///
+/// Results in a tree of routes that looks like:
+///
+/// /           --> GET, HEAD
+/// /heroes     --> GET, HEAD
+///
+/// If no match for a request is found a 404 will be returned. Both the HTTP verb and the request
+/// path are considered when determining if the request matches a defined route.
 fn router() -> Router {
-    use handlers::*;
+    use self::handlers::*;
 
     build_simple_router(|route| {
         route.get_or_head("/").to(index);
-        route.get_or_head("/heroes").to(heroes::index);
+        route
+            .get_or_head("/heroes")
+            .with_query_string_extractor::<QueryStringExtractor>()
+            .to(heroes::index);
     })
 }
 
